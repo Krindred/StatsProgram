1a02b5c4a1b0941ec16029c99847332847878532
Krindred <kualsop@gmail.com>
2018-03-19 11:34:40 -0400

    Initial Commit

---
+++ StatsProgram/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StatsProgram
+//
+//  Created by Karin Alsop on 3/19/18.
+//  Copyright © 2018 Karin Alsop. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
